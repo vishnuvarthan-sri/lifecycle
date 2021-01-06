272f6e0744f64ba08f4dcf2927c2cca5ef2802e2
vishnuvarthan <vishnuvarthan375@gmail.com>
2021-01-06 16:38:22 +0530

    commit

--- src/App.jsx
+++ src/App.jsx
@@ -3,62 +3,70 @@ import './App.css';
 import React, { Component } from 'react';
 import App2 from "./App2"
 class App extends Component {
-  constructor(props){
+  constructor(props) {
     super(props);
-    this.state={
-      template:"Starting",
-      data:"",
-      view:true
+    this.state = {
+      template: "Starting",
+      data: "",
+      view: true,
+      count:1
     }
   }
-  componentDidMount(){
-// this.setState({
-//   template:"Starting Now"
-// });
-alert("Welcome to Lifecycle")
+  componentDidMount() {
+    // this.setState({
+    //   template:"Starting Now"
+    // });
+    alert("Welcome to Lifecycle")
 
   }
-  componentDidUpdate(prevstate){
-    if(this.state.template !== prevstate.template){
-      alert("the Component is Updated")
-    }
+  componentDidUpdate(prevProps,prevState) {
+    // console.log(prevState,"state")
+    // if (prevState.count !== this.state.count) {
+    //   console.log(prevState.count,this.state.count)
+    //   this.setState({
+    //     template: `${prevState.count} times updated`
+    //   })
+    // }
+if(prevState.template !== this.state.template){
+  console.log("component is different")
+}
   }
-  update=()=>{
-    this.setState({
+  update = () => {
+    this.setState({  
       template:"All is well"
     })
-      }
-  next =()=>{
+  }
+  next = () => {
     this.setState({
-      view:false
+      view: false
     })
   }
- 
 
-  render(){
-  return (
-    <div className="App">
-    {this.state.view &&
-    <div>
-    <h1>LifeCycle</h1>
-      <header className="App-header">        
-        <p>
-        {this.state.template} 
-        <br/>
-        {this.state.data}
-        </p>
-       <button onClick={this.update}>Clickhere</button>
-       <button onClick={this.next}>Next</button>
-      </header>
-      </div>}
-{!this.state.view && 
-<div>
-  <App2 template={this.state.template}/>
-</div>
-}
 
-    </div>
-  );
+  render() {
+    return (
+      <div className="App">
+        {this.state.view &&
+          <div>
+            <h1>LifeCycle</h1>
+            <header className="App-header">
+              <p>
+                {this.state.template}
+                <br />
+                {this.state.data}
+              </p>
+              <button onClick={this.update}>Clickhere</button>
+              <button onClick={this.next}>Next</button>
+            </header>
+          </div>}
+        {!this.state.view &&
+          <div>
+            <App2 template={this.state.template} />
+          </div>
+        }
+
+      </div>
+    );
   }
 }
 
